f653f8935033c84174e88709de076d982f10a4de
naimsabir <gothamhush84@gmail.com>
2023-05-02 22:03:09 -0400

    forgot to delete some comments of domo stuff

--- client/helper.js
+++ client/helper.js
@@ -1,6 +1,5 @@
 const handleError = (message) => {
     document.getElementById('errorMessage').textContent = message;
-    //document.getElementById('domoMessage').classList.remove('hidden');
   };
   
   /* Sends post requests to the server using fetch. Will look for various
@@ -16,7 +15,6 @@ const handleError = (message) => {
     });
   
     const result = await response.json();
-   // document.getElementById('domoMessage').classList.add('hidden');
   
     if(result.redirect) {
       window.location = result.redirect;
@@ -32,7 +30,7 @@ const handleError = (message) => {
   };
 
   const hideError = () => {
-    //document.querySelector("#domoMessage").classList.add('hidden');
+
   };
 
   module.exports = {
